21155e4620cd4d9ceb288a6ede9695555dfbc160
habibullah22 <habib.bd.pro.2025@gmail.com>
2025-09-27 20:10:15 +0600

    aded cards section

--- assignment-7/src/App.jsx
+++ assignment-7/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Footer from './Footer'
+import Cards from './components/Cards'
+
+const fetchIssues = async() => {
+  const result = await fetch('/data.json')
+ return result.json();
+};
 
 function App() {
+
+  const fetchPromise = fetchIssues();
 
 
   return (
@@ -14,6 +22,10 @@ function App() {
      
       <Navbar></Navbar>
       <Banner></Banner>
+
+    <Suspense fallback={<p>Loading...</p>}>
+      <Cards fetchPromise={fetchPromise}></Cards>
+    </Suspense>
 
       <Footer></Footer>
      
---
+++ assignment-7/src/components/Cards.jsx
@@ -0,0 +1,44 @@
+import React, { use } from "react";
+
+const Cards = ({ fetchPromise }) => {
+  const initialData = use(fetchPromise);
+  console.log(initialData);
+
+  return (
+    <div className="my-16">
+      <h2 className="text-2xl font-semibold container mx-auto">
+        Customer Tickets
+      </h2>
+      <div className="grid grid-cols-2 gap-4 p-10 w-7/9">
+        {initialData.map((issue) => {
+        console.log(issue);
+        return (
+          <div className="shadow-md rounded-md cursor-pointer bg-slate-100 text-black p-7">
+            <div className="">
+              <div className="flex justify-between items-center">
+                <h2 className="text-[17px] font-semibold">{issue.Title}</h2>
+                <h2 className={`btn rounded-full ${issue.Status == "Open" ? "bg-green-500 text-black" : issue.Status == "In- Progress" ? "bg-yellow-200 text-red-900" : "bg-amber-800 text-white"}`}>
+                  {issue.Status}
+                </h2>
+              </div>
+              <p className="my-3 text-[15px]">{issue.Description}</p>
+              <div className="flex justify-between">
+                <div className="flex gap-2 text-[12px]">
+                  <p className="">{issue.ID}</p>
+                  <h3 className={` ${issue.Priority == "HIGH PRIORITY" ? "text-red-600" : issue.Priority == "LOW PRIORITY" ? "text-green-400" : issue.Priority == "MEDIUM PRIORITY" ? "text-yellow-400" : "text-black"} `}>{issue.Priority}</h3>
+                </div>
+                <div className="flex  gap-2 text-[12px]">
+                  <p>{issue.Assignee}</p>
+                  <p>{issue.Date}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
